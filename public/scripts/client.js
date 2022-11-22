048c3efb606b818ddb050328c155306a50e2f7ff
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-11-22 00:39:07 +0000

    fetch tweets/Ajax & displaying time passed/timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,8 +5,6 @@
  */
 
 // Fake data taken from initial-tweets.json
-$(document).ready(function() {
-
 const data = [
   {
     "user": {
@@ -32,46 +30,60 @@ const data = [
   }
 ]
 
-$('#submit-tweet').submit( (event) => {
-  event.preventDefault();
 
-  $.ajax({
-    type: "POST",
-    data: $('#tweet-text').serialize(),
-    url: "/tweets",
+$(document).ready(function() {
+
+  const loadTweets = function() {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      success: (response) => {
+        renderTweets(response);
+      }
+    });
+  };
+
+  loadTweets();
+
+  $('#submit-tweet').submit( (event) => {
+    event.preventDefault();
+
+    $.ajax({
+      type: "POST",
+      data: $('#tweet-text').serialize(),
+      url: "/tweets",
+    });
+
   });
 
-});
-
-const renderTweets = function(tweets) {
-  $('#tweets-container').empty();
-  // loops through tweets
-  for (const item of tweets) {
-    // calls createTweetElement for each tweet
-    let tweetElement = createTweetElement(item);
-    console.log(tweetElement);
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').append(tweetElement);
+  const renderTweets = function(tweets) {
+    $('#tweets-container').empty();
+    // loops through tweets
+    for (const item of tweets) {
+      // calls createTweetElement for each tweet
+      item.created_at = timeago.format(item.created_at);
+      let tweetElement = createTweetElement(item);
+      console.log(tweetElement);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append(tweetElement);
+    };
   };
-};
 
-const createTweetElement = function (tweet) {
-  let tweetContainer = `
-        <article class="tweet-container">
-          <header>
-            <div class="user-avatar"><img src="${tweet.user.avatars}" alt="img"><b>&nbsp;${tweet.user.name}</b></div>
-            <div class="user-handle">${tweet.user.handle}</div>
-          </header>
-          <p class="tweet-text">${tweet.content.text}</p>
-          <footer>
-            <div>${tweet.created_at}</div>
-            <div class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>
-          </footer>
-        </article>
-  `;
-  return tweetContainer;
-};
-
-renderTweets(data);
+  const createTweetElement = function (tweet) {
+    let tweetContainer = `
+          <article class="tweet-container">
+            <header>
+              <div class="user-avatar"><img src="${tweet.user.avatars}" alt="img"><b>&nbsp;${tweet.user.name}</b></div>
+              <div class="user-handle">${tweet.user.handle}</div>
+            </header>
+            <p class="tweet-text">${tweet.content.text}</p>
+            <footer>
+              <div>${tweet.created_at}</div>
+              <div class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>
+            </footer>
+          </article>
+    `;
+    return tweetContainer;
+  };
 
 });
